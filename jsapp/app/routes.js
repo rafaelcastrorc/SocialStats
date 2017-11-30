d93adad6b078c7e1b97af741fca2e96ac47e7e60
h13knsd <harish.r1729@gmail.com>
2017-11-30 14:57:52 -0500

    Integrating all login types
    One account for all social media types

--- jsapp/app/routes.js
+++ jsapp/app/routes.js
@@ -1,51 +1,105 @@
 module.exports = function(app, passport) {
-    app.get('/', function(req, res) {
-        res.render('index.ejs'); // load the index.ejs file
-    });
-    app.get('/login', function(req, res) {
-        res.render('login.ejs', { message: req.flash('loginMessage') });
-    });
-    app.get('/signup', function(req, res) {
 
-        // render the page and pass in any flash data if it exists
-        res.render('signup.ejs', { message: req.flash('signupMessage') });
-    });
-    app.get('/profile', isLoggedIn, function(req, res) {
-        res.render('profile.ejs', {
-            user : req.user // get the user out of session and pass to template
-        });
-    });
-    app.get('/logout', function(req, res) {
-        req.logout();
-        res.redirect('/');
-    });
-    app.post('/signup', passport.authenticate('local-signup', {
-        successRedirect : '/profile', // redirect to the secure profile section
-        failureRedirect : '/signup', // redirect back to the signup page if there is an error
-        failureFlash : true // allow flash messages
-    }));
-    app.post('/login', passport.authenticate('local-login', {
-        successRedirect : '/profile', // redirect to the secure profile section
-        failureRedirect : '/login', // redirect back to the signup page if there is an error
-        failureFlash : true // allow flash messages
-    }));
-    app.get('/auth/facebook', passport.authenticate('facebook', {
-      scope : ['public_profile', 'email']
-    }));
-    app.get('/auth/facebook/callback',
-        passport.authenticate('facebook', {
-            successRedirect : '/profile',
-            failureRedirect : '/'
-    }));
-    app.get('/auth/twitter', passport.authenticate('twitter'));
-    app.get('/auth/twitter/callback',
-        passport.authenticate('twitter', {
-            successRedirect : '/profile',
-            failureRedirect : '/'
-    }));
+	app.get('/', function(req, res) {
+		res.render('index.ejs');
+	});
+	app.get('/profile', isLoggedIn, function(req, res) {
+		res.render('profile.ejs', {
+			user : req.user
+		});
+	});
+
+	app.get('/logout', function(req, res) {
+		req.logout();
+		res.redirect('/');
+	});
+
+	app.get('/login', function(req, res) {
+		res.render('login.ejs', { message: req.flash('loginMessage') });
+	});
+
+	app.post('/login', passport.authenticate('local-login', {
+		successRedirect : '/profile',
+		failureRedirect : '/login',
+		failureFlash : true
+	}));
+
+	app.get('/signup', function(req, res) {
+		res.render('signup.ejs', { message: req.flash('loginMessage') });
+	});
+
+	app.post('/signup', passport.authenticate('local-signup', {
+		successRedirect : '/profile',
+		failureRedirect : '/signup',
+		failureFlash : true
+	}));
+
+	app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+	app.get('/auth/facebook/callback',
+		passport.authenticate('facebook', {
+			successRedirect : '/profile',
+			failureRedirect : '/'
+		}));
+
+	app.get('/auth/twitter', passport.authenticate('twitter', { scope : 'email' }));
+	app.get('/auth/twitter/callback',
+		passport.authenticate('twitter', {
+			successRedirect : '/profile',
+			failureRedirect : '/'
+		}));
+
+	app.get('/connect/local', function(req, res) {
+		res.render('connect-local.ejs', { message: req.flash('loginMessage') });
+	});
+	app.post('/connect/local', passport.authenticate('local-signup', {
+		successRedirect : '/profile',
+		failureRedirect : '/connect/local',
+		failureFlash : true
+	}));
+
+	app.get('/connect/facebook', passport.authorize('facebook', { scope : 'email' }));
+
+	app.get('/connect/facebook/callback',
+		passport.authorize('facebook', {
+			successRedirect : '/profile',
+			failureRedirect : '/'
+		}));
+
+	app.get('/connect/twitter', passport.authorize('twitter', { scope : 'email' }));
+	app.get('/connect/twitter/callback',
+		passport.authorize('twitter', {
+			successRedirect : '/profile',
+			failureRedirect : '/'
+		}));
+
+	app.get('/unlink/local', function(req, res) {
+		var user = req.user;
+		user.local.email    = undefined;
+		user.local.password = undefined;
+		user.save(function(err) {
+			res.redirect('/profile');
+		});
+	});
+
+	app.get('/unlink/facebook', function(req, res) {
+		var user = req.user;
+		user.facebook.token = undefined;
+		user.save(function(err) {
+			res.redirect('/profile');
+		});
+	});
+
+	app.get('/unlink/twitter', function(req, res) {
+		var user = req.user;
+		user.twitter.token = undefined;
+		user.save(function(err) {
+			res.redirect('/profile');
+		});
+	});
 };
+
 function isLoggedIn(req, res, next) {
-    if (req.isAuthenticated())
-        return next();
-    res.redirect('/');
+	if (req.isAuthenticated())
+		return next();
+	res.redirect('/');
 }
--- jsapp/config/auth.js
+++ jsapp/config/auth.js
@@ -5,7 +5,7 @@ module.exports = {
         'clientSecret'  : '5f242f5b933ef672ef048a16a795130d', // your App Secret
         'callbackURL'   : 'http://localhost:8080/auth/facebook/callback',
         'profileURL'    : 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
-        'profileFields' : ['id', 'email', 'name'] // For requesting permissions from Facebook API
+        'profileFields' : ['id', 'name'] // For requesting permissions from Facebook API
     },
 
     'twitterAuth' : {

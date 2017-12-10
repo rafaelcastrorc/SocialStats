06a686d3c1f31d1c559434a0f7bd3bfdbc2fb765
h13knsd <harish.r1729@gmail.com>
2017-12-10 18:06:39 -0500

    navbar modif

--- ngApp/server/routes/user_routes.js
+++ ngApp/server/routes/user_routes.js
@@ -48,7 +48,7 @@ module.exports = function(app, passport) {
   });
 
 	app.get('/landing', function(req, res) {
-		res.render('index.ejs');
+    res.render('index.ejs');
 	});
 
 	app.get('/profile', isLoggedIn, function(req, res) {

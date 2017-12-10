bedc06e9a8459dbf7433f69f2778685d27bae046
h13knsd <harish.r1729@gmail.com>
2017-12-10 17:51:18 -0500

    routes change
    changing route to profile and notes if user is not logged in

--- ngApp/server/routes/user_routes.js
+++ ngApp/server/routes/user_routes.js
@@ -149,5 +149,5 @@ module.exports = function(app, passport) {
 function isLoggedIn(req, res, next) {
 	if (req.isAuthenticated())
 		return next();
-	res.redirect('/');
+	res.redirect('/landing');
 }

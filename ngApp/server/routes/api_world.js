69ac75d7e2278d4e5ee6854b701914288d7855da
Digvijay <digvijay10011@gmail.com>
2017-12-10 15:51:32 -0500

    Merge branches 'digvijay_new' and 'master' of github.com:h13knsd/socialstats into digvijay_new
    # Conflicts:
    #	ngApp/src/app/app.module.ts

--- ngApp/server/routes/api_world.js
+++ ngApp/server/routes/api_world.js
@@ -17,7 +17,7 @@ router.get('/', function (req, res) {
 //To get all indicators
 router.get('/allIndicators', function (req, res) {
   console.log('Getting all indicators');
-  var query = 'select * from Indicators';
+  var query = 'select * from Indicators order by code';
 
   console.log(query);
   connection.query(query, function(err, rows, fields) {
@@ -49,7 +49,10 @@ router.get('/query1/:country/:indicator', function (req, res) {
   const indicator = req.params.indicator;
 
   console.log('Q1: Getting data for '+country+' for '+indicator);
-  var query = "select y2000, y2001, y2002, y2003, y2004, y2005 from WorldBank where country_code = '"+ country +"' and" +
+  // var query = "select y2000, y2001, y2002, y2003, y2004, y2005, y2006, y2007, y2008, y2009, y2010, y2011, y2012," +
+  //   " y2013, y2014 from WorldBank where country_code = '"+ country +"' and" +
+  //   " indicator_code = '" + indicator + "';";
+  var query = "select * from WorldBank where country_code = '"+ country +"' and" +
     " indicator_code = '" + indicator + "';";
 
   console.log(query);
@@ -63,6 +66,31 @@ router.get('/query1/:country/:indicator', function (req, res) {
 
 
 });
+
+router.get('/query3/:country/:year', function (req, res) {
+
+  const country = req.params.country;
+  var year = req.params.year;
+  year = 'y' + year ;
+  console.log('year: '+year);
+
+  console.log('Q1: Getting data for '+country+' for '+year);
+
+  var query = "select "+year+" from WorldBank where country_code = '"+ country + "' order by indicator_code;";
+
+  console.log(query);
+  connection.query(query, function(err, rows, fields) {
+    if (err) console.log(err);
+    else {
+      console.log(rows);
+      res.json(rows);
+    }
+  });
+
+
+});
+
+
 
 
 

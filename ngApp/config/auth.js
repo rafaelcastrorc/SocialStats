8c0f54ccf0ac8468e067116438e29506431e4ee6
h13knsd <harish.r1729@gmail.com>
2017-12-12 15:50:19 -0500

    cleanup

--- ngApp/config/auth.js
+++ ngApp/config/auth.js
@@ -1,11 +1,11 @@
 module.exports = {
 
     'facebookAuth' : {
-        'clientID'      : '495523660828633', // your App ID
-        'clientSecret'  : '5f242f5b933ef672ef048a16a795130d', // your App Secret
+        'clientID'      : '495523660828633',
+        'clientSecret'  : '5f242f5b933ef672ef048a16a795130d',
         'callbackURL'   : 'http://localhost:3000/auth/facebook/callback',
         'profileURL'    : 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
-        'profileFields' : ['id', 'name'] // For requesting permissions from Facebook API
+        'profileFields' : ['id', 'name']
     },
 
     'twitterAuth' : {

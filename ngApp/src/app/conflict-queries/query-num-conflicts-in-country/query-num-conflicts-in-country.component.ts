78ce4bfb9e55f25b283117ed554b915190e60864
Yash Palkhiwala <15ypalkhiwala@gmail.com>
2017-12-12 19:29:48 -0500

    Begin graphs

--- ngApp/src/app/conflict-queries/query-num-conflicts-in-country/query-num-conflicts-in-country.component.ts
+++ ngApp/src/app/conflict-queries/query-num-conflicts-in-country/query-num-conflicts-in-country.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewChild } from '@angular/core';
 import {Country} from '../../countryAWS';
 import {HttpClient} from "@angular/common/http";
+import {ChartsModule} from 'chart.js';
 
 interface RawDataPacket {
   year: number;
@@ -20,6 +21,30 @@ export class QueryNumConflictsInCountryComponent implements OnInit {
   hasSelectedCountry = false;
   displayAlert = false;
   queryConflictsCountry: RawDataPacket[];
+  lineChartLabels = [];
+  numConflicts = [];
+  lineChartOptions = {
+    legend: {
+      display: false
+    },
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        },
+        scaleLabel: {
+          display: true,
+          labelString: 'Number of conflicts'
+        }
+      }]
+    }
+  };
+  lineChartData = [{data: [], label:'Num Conflicts'}];
+  lineChartType = 'line';
+  displayChart = false;
+  displayNoData = false;
 
   constructor(private http: HttpClient) { }
 
@@ -35,18 +60,42 @@ export class QueryNumConflictsInCountryComponent implements OnInit {
   }
 
   onSubmit() {
+    this.displayNoData = false;
     if (this.hasSelectedCountry) {
+      this.displayChart = false;
       this.http.get<RawDataPacket[]>('/api_aws/conflictsPerYear/' + this.selectedCountryCode
       ).subscribe(data => {
         this.queryConflictsCountry = data;
+        if (this.queryConflictsCountry.length < 1) {
+          this.displayNoData = true;
+        } else {
+          this.formatGraphData(data);
+        }
       });
     } else {
       this.displayAlert = true;
     }
   }
 
+  formatGraphData(data) {
+    this.lineChartLabels = [];
+    this.numConflicts = [];
+    for (let index = 0; index < data.length; index++) {
+      this.lineChartLabels.push(data[index].year);
+      this.numConflicts.push(data[index].numConflicts);
+    }
+
+    this.lineChartData[0].data = this.numConflicts;
+    console.log(this.lineChartLabels, this.numConflicts);
+    this.displayChart = true;
+  }
+
   dismissAlert() {
     this.displayAlert = false;
+  }
+
+  dismissNoData() {
+    this.displayNoData = false;
   }
 
 }
